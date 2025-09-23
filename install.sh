5c2f82eeda85c69a4e302bb0b0a7d45a666d24ef
Yeshey <yesheysangpo@hotmail.com>
2025-09-23 23:03:29 +0200

    one more step

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 # Setup PULP-RISC-V-GCC-TOOLCHAIN
-wget https://github.com/pulp-platform/pulp-riscv-gnu-toolchain/releases/download/v1.0.16/v1.0.16-pulp-riscv-gcc-ubuntu-18.tar.bz2
+wget --progress=dot:mega https://github.com/pulp-platform/pulp-riscv-gnu-toolchain/releases/download/v1.0.16/v1.0.16-pulp-riscv-gcc-ubuntu-18.tar.bz2
 tar -xvf v1.0.16-pulp-riscv-gcc-ubuntu-18.tar.bz2 -C ./
 mv v1.0.16-pulp-riscv-gcc-ubuntu-18 pulp-riscv-gcc-toolchain
 rm v1.0.16-pulp-riscv-gcc-ubuntu-18.tar.bz2
